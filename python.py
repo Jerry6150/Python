f077d021f5c455d4382c7e1b311dcfb32078b6d6
Jerry6150 <kijun6150@snu.ac.kr>
2025-03-03 05:03:51 +0000

    raooooa

---
+++ python.py
@@ -0,0 +1,15 @@
+
+#name = input("올해 년도를 입력해주세요.")
+#x = int(name)-1
+#print("작년은 ", x ,"년입니다.")
+
+#A = int(input("숫자 1을 입력해주세요."))
+#B = int(input("숫자 2를 입력해주세요."))
+#print(A!=B)
+#print(B//A)
+#print(A**B>=B)
+#print(not A!=B)
+
+import math
+x = float(input("숫자를 입력해주세요."))
+print(math.sqrt(x))
